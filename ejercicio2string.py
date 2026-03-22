9af99d4c0154d184d4c545359a052eff197b4b68
20071407 <pl033503202@unab.edu.sv>
2026-03-21 21:10:39 -0600

    solicita una frace muestra la frace original,en mayuscula y minuscula

---
+++ ejercicio2string.py
@@ -0,0 +1,13 @@
+
+
+
+frase = input("Ingrese una frase: ")
+
+print(frase)
+
+frase_Mayusculas = frase.upper()
+print(frase_Mayusculas)
+
+frase_minusculas = frase.lower()
+print(frase_minusculas)
+
