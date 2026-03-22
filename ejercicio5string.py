304c3d857560ee3f61b3b57118da9dd45e80a73c
20071407 <pl033503202@unab.edu.sv>
2026-03-21 21:40:04 -0600

    solicita una frace y muestra la primera y ultima letra de la frase

---
+++ ejercicio5string.py
@@ -0,0 +1,8 @@
+
+frase = input("Ingrese una frase: ")
+
+frase_primeraLetra = frase[0]
+frase_ultimaLetra = frase[-1]
+
+print(f"La primera letra es: {frase_primeraLetra}")
+print(f"La última l es: {frase_ultimaLetra}")
