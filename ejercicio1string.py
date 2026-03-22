e0ff0e0c23f0a4cae6e9e622476f6de4aaca212f
20071407 <pl033503202@unab.edu.sv>
2026-03-21 21:08:02 -0600

    solicita un nombre lo pone en mayusculas,minucula y la cantidad de caracteres

---
+++ ejercicio1string.py
@@ -0,0 +1,11 @@
+
+
+nombre = input("Ingrese su nombre completo: ")
+
+nombre_Mayusculas = nombre.upper()
+print(nombre_Mayusculas)
+
+nombre_minuscula = nombre.lower()
+print(nombre_minuscula)
+
+print(f"La cantidad de caracteres es =: {len(nombre)}")
