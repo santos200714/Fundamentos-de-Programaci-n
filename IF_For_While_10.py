f36bb4611c0357881c5986de0f1cab5958c3ab03
20071407 <pl033503202@unab.edu.sv>
2026-04-18 22:42:00 -0600

    semana 12,ejercicio 10

---
+++ IF_For_While_10.py
@@ -0,0 +1,7 @@
+usuario = input("Ingrese su usuario: ")
+contraseña = input("Ingrese su contraseña: ")
+
+if usuario == "santos" and contraseña == "1234":
+    print("Acceso permitido.")
+else:
+    print("Acceso denegado.")
